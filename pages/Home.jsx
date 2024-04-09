1493ac91f9bd83f913b7684e4026c65f528a2824
Qingjie Shen (Clark) <shen0126@algonquinlive.com>
2024-04-09 00:37:52 -0400

    vite install

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -3,8 +3,8 @@ import React from "react"
 export default function Home() {
     return (
         <div className="home-container">
-            <h1>You got the travel plans, we got the travel vans.</h1>
-            <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+            <h1>Some Sprites are visiting your home</h1>
+            <p>Try to catch them.</p>
         </div>
     )
-};
+}
