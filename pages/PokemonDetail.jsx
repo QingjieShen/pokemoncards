de536ed5a250a6703929fbd51b9a97fd9caa553d
Qingjie Shen (Clark) <shen0126@algonquinlive.com>
2024-04-10 01:24:47 -0400

    adds basic info in the pokemon detail page

--- pages/PokemonDetail.jsx
+++ pages/PokemonDetail.jsx
@@ -2,8 +2,46 @@ import React from "react";
 import { Link, useParams, useLocation } from "react-router-dom"
 
 export default function PokemonDetail() {
+    const pokemonId = useParams()
+    const [currentPokemonDetail, setCurrentPokemonDetail] = React.useState(null)
+    const localPokemons = JSON.parse(localStorage.getItem("myPokemons"))
+    const currentPokemon = findPokemon(pokemonId.id)
+    const detailUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonId.id}/`
+    // console.log(currentPokemon)
+    React.useEffect(() => {
+        async function getPokemonDetails() {
+            const res = await fetch(detailUrl)
+            const data = await res.json()
+            console.log(data)
+            setCurrentPokemonDetail(data)
+        }
+        getPokemonDetails()
+    }, [])
+
+    // this function can be defined as a utility function
+    function findPokemon(id) {
+        let targetPokemon = null
+        localPokemons.map(pokemon => {
+            if (pokemon.index === id) {
+                targetPokemon = pokemon
+            }
+        })
+        return targetPokemon
+    }
 
     return (
-        <h1>Pokemon Detail Page</h1>
+        <div className="details-container">
+            <Link to="../" relative="path" className="back-btn">
+                Back to all Pokemons
+            </Link>
+            <div className="pokemon-details">
+                <img className="detail-front-img" src={currentPokemon.url} alt="" />
+                <h3 className="detail-name">{currentPokemon.name.charAt(0).toUpperCase() + currentPokemon.name.slice(1)}</h3>
+                <div className="abilities">
+
+                </div>
+            </div>
+        </div>
+        
     )
 }
