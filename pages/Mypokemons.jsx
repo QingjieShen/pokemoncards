d6ad3e61447ff4e48c1f21933a452347a7aba831
Qingjie Shen (Clark) <shen0126@algonquinlive.com>
2024-04-12 17:30:34 -0400

    adds types filter btns

--- pages/Mypokemons.jsx
+++ pages/Mypokemons.jsx
@@ -3,15 +3,21 @@ import { Link, useSearchParams } from "react-router-dom"
 
 
 export default function MyPokemons() {
-    // const [searchParams, useSearchParams] = useSearchParams()
+    const [searchParams, setSearchParams] = useSearchParams()
+    const typeFilter = searchParams.get("type")
 
-    // const typeFilter = searchParams.get("type")
     const localPokemons = JSON.parse(localStorage.getItem("myPokemons")) || []
-    // console.log(localPokemons)
+    const displayPokemon = localPokemons.length > 0 ? (typeFilter ? localPokemons.filter(poke => {
+        poke.types.map(type => {
+            if (type.name === typeFilter) {
+                return true
+            }
+        })
+    }) : localPokemons) : localPokemons
     let pokemonsElements = []
 
-    if(localPokemons.length > 0) {
-        pokemonsElements= localPokemons.map(localPokemon => (
+    if(displayPokemon.length > 0) {
+        pokemonsElements= displayPokemon.map(localPokemon => (
             <div key={localPokemon.index} className="my-pokemon-unit">
                 <Link to={localPokemon.index}>
                     <img src={localPokemon.url} />
@@ -21,10 +27,83 @@ export default function MyPokemons() {
         ))
     }
 
+    function handleFilterChange(key, value) {
+        setSearchParams(prevParams => {
+            if (!value) {
+                prevParams.delete(key)
+            } else {
+                prevParams.set(key, value)
+            }
+            return prevParams
+        })
+    }
+
     return (
         <div>
             <div className="my-pokemons-wrapper">
-                {localPokemons.length > 0 ? <h1>Here are your Pokemon</h1> : <h1>You don't have any Pokemon</h1>}
+                {displayPokemon.length > 0 ? <h1>Here are your Pokemon</h1> : <h1>You don't have any Pokemon</h1>}
+                <div className="pokemon-type-filter-btns">
+                    <button onClick={() => handleFilterChange("type", "normal")} className="pokemon-type-normal">
+                        Normal
+                    </button>
+                    <button onClick={() => handleFilterChange("type", "fighting")} className="pokemon-type-fighting">
+                        Fighting
+                    </button>
+                    <button onClick={() => handleFilterChange("type", "flying")} className="pokemon-type-flying">
+                        Flying
+                    </button>
+                    <button onClick={() => handleFilterChange("type", "poison")} className="pokemon-type-poison">
+                        Poison
+                    </button>
+                    <button onClick={() => handleFilterChange("type", "ground")} className="pokemon-type-ground">
+                        Ground
+                    </button>
+                    <button onClick={() => handleFilterChange("type", "rock")} className="pokemon-type-rock">
+                        Rock
+                    </button>
+                    <button onClick={() => handleFilterChange("type", "bug")} className="pokemon-type-bug">
+                        Bug
+                    </button>
+                    <button onClick={() => handleFilterChange("type", "ghost")} className="pokemon-type-ghost">
+                        Ghost
+                    </button>
+                    <button onClick={() => handleFilterChange("type", "steel")} className="pokemon-type-steel">
+                        Steel
+                    </button>
+                    <button onClick={() => handleFilterChange("type", "fire")} className="pokemon-type-fire">
+                        Fire
+                    </button>
+                    <button onClick={() => handleFilterChange("type", "water")} className="pokemon-type-water">
+                        Water
+                    </button>
+                    <button onClick={() => handleFilterChange("type", "grass")} className="pokemon-type-grass">
+                        Grass
+                    </button>
+                    <button onClick={() => handleFilterChange("type", "electric")} className="pokemon-type-electric">
+                        Electric
+                    </button>
+                    <button onClick={() => handleFilterChange("type", "psychic")} className="pokemon-type-psychic">
+                        Psychic
+                    </button>
+                    <button onClick={() => handleFilterChange("type", "ice")} className="pokemon-type-ice">
+                        Ice
+                    </button>
+                    <button onClick={() => handleFilterChange("type", "dragon")} className="pokemon-type-dragon">
+                        Dragon
+                    </button>
+                    <button onClick={() => handleFilterChange("type", "dark")} className="pokemon-type-dark">
+                        Dark
+                    </button>
+                    <button onClick={() => handleFilterChange("type", "fairy")} className="pokemon-type-fairy">
+                        Fairy
+                    </button>
+                    <button onClick={() => handleFilterChange("type", "unknown")} className="pokemon-type-unknown">
+                        Unknown
+                    </button>
+                    <button onClick={() => handleFilterChange("type", "shadow")} className="pokemon-type-shadow">
+                        Shadow
+                    </button>
+                </div>
                 {pokemonsElements.length > 0 ?(<div className="pokemons-gallery">
                     {pokemonsElements}
                 </div>) : (
